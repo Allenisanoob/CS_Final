e4f28c180a3c888005f9781f42633181df399d10
69-cpu <79269418+69-cpu@users.noreply.github.com>
2023-12-26 12:11:02 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import pygame
 from process import *
 
 def main():
-    # pygame setup
     pygame.init()
+    pygame.mixer.init()
     pygame.display.set_caption("Eels and Escalators")
-
-    screen = pygame.display.set_mode((1280, 720))
+    
+    screen = pygame.display.set_mode((784, 593))
     clock = pygame.time.Clock()
 
     game = process(screen, clock)
     game.run()
-    
+
+
 if __name__ == "__main__":
     main()
