054ad104db8d6633028d849baa7e653c0e7a5c1a
69-cpu <79269418+69-cpu@users.noreply.github.com>
2023-12-26 12:11:28 +0800

    Add files via upload

--- scenes/start_menu.py
+++ scenes/start_menu.py
@@ -6,28 +6,33 @@ class scene:
         self.screen = screen
         self.clock = clock
         self.running = True
-        
+
         self.done = False
         self.next = [ready_menu.scene]
         self.background = pygame.image.load("resources\\start_menu_temp.png")
-
+        
+        self.mouse = Mouse()
+        self.allsprites = pygame.sprite.Group(self.mouse)
+        pygame.mouse.set_visible(False)
+        
     def run(self):
         while self.running and not self.done:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-            
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    pygame.mixer.music.load("resources\\sounds\\laugh.mp3")
+                    pygame.mixer.music.play()
+
             condition = False
             if condition:
                 self.done = True
             
             self.render()
-
             pygame.display.flip()
-
             self.clock.tick(60)
             
-        if self.running == False:
+        if not self.running:
             return 0
         elif self.done and condition:
             return self.next[0]
@@ -35,4 +40,22 @@ class scene:
             return -1
 
     def render(self):
-        pygame.Surface.blit(self.screen, self.background, (0, 0))
+        self.screen.blit(self.background, (0, 0))
+        self.allsprites.update()
+        self.allsprites.draw(self.screen)
+
+class Mouse(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = self.load_image('resources\\cursor_temp.png')
+        self.rect.topleft = (0, 0)  
+    
+    def update(self):
+        self.rect.topleft = pygame.mouse.get_pos()
+
+    def load_image(self, name):
+        image = pygame.image.load(name)
+        image = image.convert()
+        colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey, pygame.RLEACCEL)
+        return image, image.get_rect()
