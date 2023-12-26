841b4d3d943db99c21ab231b54e3609e424072b5
69-cpu <79269418+69-cpu@users.noreply.github.com>
2023-12-26 21:00:02 +0800

    Add files via upload

--- scenes/start_menu.py
+++ scenes/start_menu.py
@@ -22,12 +22,12 @@ class scene:
         #Creating start button
         self.button_0 = button(self.screen, image = "resources\\start_button.png")
         self.button_0.x = screen.get_width() // 2 - self.button_0.width // 2
-        self.button_0.y = screen.get_height() // 2 - self.button_0.height // 2 -20
+        self.button_0.y = screen.get_height() // 2 - self.button_0.height // 2 -40
         
         #Creating rule button
         self.button_1 = button(self.screen, image = "resources\\rule_button.png")
         self.button_1.x = screen.get_width() // 2 - self.button_1.width // 2
-        self.button_1.y = screen.get_height() // 2 - self.button_1.height // 2 + 50
+        self.button_1.y = screen.get_height() // 2 - self.button_1.height // 2 + 30
         
     def run(self):
         while self.running and not self.done:
