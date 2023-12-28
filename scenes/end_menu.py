1b31ad0ffeee51447136344846b5cbff7b56bdc3
69-cpu <79269418+69-cpu@users.noreply.github.com>
2023-12-28 10:58:58 +0800

    Add files via upload

--- scenes/end_menu.py
+++ scenes/end_menu.py
@@ -1,50 +1,57 @@
 import pygame
+from library.button import *
+from library.mouse import *
 import scenes.ready_menu as ready_menu
-#import next_scenes
 
-#Calculations and game logic should be put here.
 def game_logic():
     pass
-
 class scene:
     def __init__(self, screen, clock, *args):
         self.screen = screen
         self.clock = clock
         self.running = True
-        
-        #Ture if the scene is ended by regular procedure.
-        self.done = False
-        
-        #Put all the "next scenes" in this list.
-        self.next = []
 
+        self.done = False
+        self.next = [ready_menu.scene]
+        self.background = pygame.image.load("resources\\end_menu.png")
+        
+        self.mouse = Mouse()
+        self.allsprites = pygame.sprite.Group(self.mouse)
+        pygame.mouse.set_visible(False)
+        
+        self.button_0 = button(self.screen, image = "resources\\restart_button.png")
+        self.button_0.x = screen.get_width() // 2 - self.button_0.width // 2 - 250
+        self.button_0.y = screen.get_height() // 2 - self.button_0.height // 2 -220
+
+        self.button_1 = button(self.screen, image = "resources\\quit_button.png")
+        self.button_1.x = screen.get_width() // 2 - self.button_1.width // 2 + 250
+        self.button_1.y = screen.get_height() // 2 - self.button_1.height // 2 - 220
+        
     def run(self):
         while self.running and not self.done:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
             
-            #Put the condition to end the scene here, if there are more than one, use "and" or "or" to combine them.
-            condition = False
-            if condition:
+            if self.button_0.clicked or self.button_1.clicked:
                 self.done = True
             
-            game_logic()
-            
             self.render()
-
             pygame.display.flip()
-
             self.clock.tick(60)
             
-        if self.running == False:
+        if not self.running:
             return 0
-        #Use the condition to choose the next scene here, if there are more than one, use multiple elif.
-        elif self.done and condition:
-            return self.next[0]
+        elif self.done and self.button_0.clicked:
+            return [self.next[0]]
+        elif self.done and self.button_1.clicked:
+            return -1
         else:
             return -1
 
-    #Put all the renderings here.
     def render(self):
-        pass
+        self.screen.blit(self.background, (0, 0))
+        self.button_0.draw()
+        self.button_1.draw()
+        self.allsprites.update()
+        self.allsprites.draw(self.screen)
